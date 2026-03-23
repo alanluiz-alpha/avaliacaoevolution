5b792bc7d576a9fee1dade617c7f1849909864b8
alanluiz-alpha <alan.luiz180@gmail.com>
2026-03-23 08:47:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/",   // <<< MUDA AQUI
   server: {
     host: "::",
     port: 8080,
@@ -31,21 +30,6 @@ export default defineConfig(({ mode }) => ({
             sizes: "512x512",
             type: "image/jpeg",
             purpose: "any maskable",
-          },
-        ],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) => request.destination === "image",
-            handler: "CacheFirst",
-            options: {
-              cacheName: "images-cache",
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 30,
-              },
-            },
           },
         ],
       },
